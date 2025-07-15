2d69ffc7bfc17d33654278bd2a64ff72f76cb233
Florian <rogoschflorian@outlook.com>
2025-07-15 20:16:07 +0200

    First Stable Version

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
-# Pelican Install Script with SSL
-# ❗NOT FINISHED YET❗
-## for now only for personal use public version in progress                                              
+# Pelican Panel Installer (Bash Script)
+
+A simple Bash script to install the **Pelican Panel** and **Wings**, with support for SSL via Certbot, MySQL integration, and automatic Apache configuration.
+
+## 🛠️ Features
+
+- 🔧 **Flexible Installation**  
+  Choose to install the **Panel**, **Wings**, or **both** independently.
+
+- 😎 **Simple Installation Guide**  
+  This Bash Scirpt uses whiptail for a better userfriendly experience.
+  
+
+- 🔐 **SSL Support via Certbot**  
+  Automatically obtain and configure HTTPS certificates using Let's Encrypt.
+
+- 🗃️ **MySQL Support**  
+  Automatically installs and configures MySQL for the Panel backend.
+
+- 🌐 **Apache Auto Configuration**  
+  Automatically creates a working Apache VirtualHost for your domain.
+
+---
+
+## 📦 Requirements
+
+
+| Distribution       | Recommended Version(s) | Support Status | Notes                            |
+|--------------------|------------------------|----------------|----------------------------------|
+| **Ubuntu**         | 24.04/24.10        | ✅ Supported    | Fully tested                     |
+| **Debian**         | 11, 12                  | ❌ Not Supported yet    | Soon        |
+|||||
+
+
+## Other Requirements
+### This script requires root/superuser access
+- Git should be installed
+   ```bash
+    apt install git
+   ```
+## 🚀 Installation
+
+1. Download the installer:
+   ```bash
+    git clone https://github.com/Kabel6499/pelican
+   ```
+2. Go to the directory of the installer and run:
+    ```bash
+     chmod 777 installer.sh
+     bash installer.sh
+     ```
+---
+
+# 🏁 After Wings Installation
+### 1. Go to your Panel and open the Nodes Tab in Admin view
+### 2. Create New Node and Enter your Node IP as Domain Name
+### 3. Click Next and Click Create Node
+### 4. Then Scroll down and Create your Allocations
+### 5. Submit
+### 6. Go to Configuration File Click on Autodebloy Command
+### 7. Click on Standalone and Copy the Command
+### 8. Paste it into your Terminal
+### 9. Click enter and run:
+```bash
+sudo wings --debug
+```
+### 10. Wait 10 seconds and click ctrl+c on your keyboard
+### 11. Then Check on your node page if your node is online
+### 12. After this run these commands:
+```bash
+sudo systemctl enable --now wings
+```
+
+## ❓Questions
+### If you have any further questions create an Issue on this Repository!
+
+# ❗Disclaimer:
+## This script is an unofficial community project and is not affiliated with the Pelican team.
--- install.sh
+++ install.sh
@@ -1,87 +1,335 @@
 #!/bin/bash
 
-## Update Debian BAsed System
+export NEWT_COLORS='
+root=,blue
+window=,black
+border=white,black
+textbox=white,black
+button=black,white
+entry=,black
+checkbox=,black
+compactbutton=,black
+'
 
-apt update
-apt upgrade -y
+installation=$(whiptail --title "Installation" \
+  --menu "What do you want to install?" 15 60 4 \
+  "1" "Panel" \
+  "2" "Wings" \
+  "3" "Both" \
+  3>&1 1>&2 2>&3)
 
-echo The System is now Updated
+case "$installation" in
+  1)
+    panel_domain=$(whiptail --title "Panel Domain" --inputbox "What is your Panel Domain" 10 60 3>&1 1>&2 2>&3)
 
-## Install Apache2
+    certbot_mail=$(whiptail --title "Certbot E-Mail" --inputbox "Please enter your E-Mail" 10 60 3>&1 1>&2 2>&3)
 
-apt install apache2 -y
-## Install PHP 8.4 with all debendencies 
+    root_pw=$(whiptail --title "Create DB Password" \
+    --passwordbox "What do you want as your Root Acc DB Password?" 10 60 3>&1 1>&2 2>&3)
 
-## Adding PHP Repo
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php -y
+    panel_pw=$(whiptail --title "Create DB Password for Panel Acc" \
+    --passwordbox "What do you want as your Panel Acc DB Password?" 10 60 3>&1 1>&2 2>&3)
 
-## Install PHP 8.4
-sudo apt install php8.4 php8.4-fpm -y
+    pelican_db=$(whiptail --title "Panel Database Name" --inputbox "What you want to name your Panel Databse?" 10 60 "panel" 3>&1 1>&2 2>&3)
 
-## Installing all PHP dependencies for Pelican Panel
+    panel_db_user=$(whiptail --title "Panel DB Username" --inputbox "What should be your DB Username for your Panel" 10 60 "pelican" 3>&1 1>&2 2>&3)
 
-sudo apt install php8.4-{gd,mysql,mbstring,bcmath,xml,curl,zip,intl,sqlite3,fpm} -y
+    whiptail --title "Overview" --msgbox "$(echo -e "Panel Domain: $panel_domain\nSSL Mail: $certbot_mail\nDatabase Type: MySQL\nPanel Database Password: $panel_pw\nPanel Database Name: $pelican_db\nRoot User DB Password: $root_pw\nDatabase User: $panel_db_user")" 15 60
 
-## Instaling other dependencies for pelican
+    ## Update Debian Based Systems
+    apt update
+    apt upgrade -y
 
-apt install curl
-apt install tar
-apt install unzip
+    ## Install Apache2
+    apt install apache2 -y
 
-## Installing Database Server
+    ## Install PHP 8.4 with all dependencies 
+    apt install software-properties-common -y
+    add-apt-repository ppa:ondrej/php -y
+    apt install -y php8.4 php8.4-fpm php8.4-gd php8.4-mysql php8.4-mbstring php8.4-bcmath php8.4-xml php8.4-curl php8.4-zip php8.4-intl php8.4-sqlite3 libapache2-mod-php8.4
 
-apt install mysql-server
+    ## Installing other dependencies for pelican
+    apt install curl -y
+    apt install tar -y
+    apt install unzip -y
 
-## Creating Directory and Download Pelican Repo
+    ## Installing Database Server
+    echo "mysql-server mysql-server/root_password password $root_pw" | sudo debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password $root_pw" | sudo debconf-set-selections
+
+    apt install mysql-server -y
+
+    mysql -u root -p"$root_pw" <<EOF
+CREATE DATABASE IF NOT EXISTS $pelican_db;
+CREATE USER IF NOT EXISTS '$panel_db_user'@'localhost' IDENTIFIED BY '$panel_pw';
+GRANT ALL PRIVILEGES ON $pelican_db.* TO '$panel_db_user'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+    ## Creating Directory and Download Pelican Repo
+    sudo mkdir -p /var/www/pelican
+    cd /var/www/pelican
+
+    curl -L https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz | sudo tar -xzv
+
+    ## Installing Composer
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+    sudo COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
+
+    ## Installing Certbot / SSL Certificates
+    sudo apt install -y python3-certbot-apache
+
+    certbot --apache \
+    --non-interactive \
+    --agree-tos \
+    --redirect \
+    --e
+    mail "$certbot_mail" \
+    -d "$panel_domain"
+
+    ## Disable Default Site
+    a2dissite 000-default default-ssl 000-default-le-ssl
+
+    cat <<EOF > /etc/apache2/sites-available/pelican.conf
+<VirtualHost *:80>
+    ServerName $panel_domain
+
+    RewriteEngine On
+    RewriteCond %{HTTPS} !=on
+    RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L] 
+</VirtualHost>
+
+<VirtualHost *:443>
+    ServerName $panel_domain
+    DocumentRoot "/var/www/pelican/public"
+
+    AllowEncodedSlashes On
+
+    php_value upload_max_filesize 100M
+    php_value post_max_size 100M
+
+    <Directory "/var/www/pelican/public">
+        Require all granted
+        AllowOverride all
+    </Directory>
+
+    SSLEngine on
+    SSLCertificateFile /etc/letsencrypt/live/$panel_domain/fullchain.pem
+    SSLCertificateKeyFile /etc/letsencrypt/live/$panel_domain/privkey.pem
+</VirtualHost>
+EOF
+
+    sudo a2ensite pelican.conf
+    sudo a2enmod ssl
+    sudo a2enmod rewrite
+    sudo a2enmod php8.4
+
+    sudo systemctl restart apache2
+
+    php artisan p:environment:setup
+
+    sudo chmod -R 755 storage/* bootstrap/cache/
+
+    sudo chown -R www-data:www-data /var/www/pelican
+    ;;
+
+  2)
+    node_domain=$(whiptail --title "Node Domain" --inputbox "What is your Node Domain" 10 60 3>&1 1>&2 2>&3)
+
+    certbot_mail=$(whiptail --title "Certbot E-Mail" --inputbox "Please enter your E-Mail" 10 60 3>&1 1>&2 2>&3)
+
+    whiptail --title "Overview" --msgbox "$(echo -e "Node Domain: $node_domain\nSSL Mail: $certbot_mail")" 15 60
+
+    certbot certonly --apache \
+    --non-interactive \
+    --agree-tos \
+    --redirect \
+    --email "$certbot_mail" \
+    -d "$node_domain"
+
+    curl -sSL https://get.docker.com/ | CHANNEL=stable sudo sh
+
+    sudo mkdir -p /etc/pelican /var/run/wings
+    sudo curl -L -o /usr/local/bin/wings "https://github.com/pelican-dev/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+    sudo chmod u+x /usr/local/bin/wings
 
 
-sudo mkdir -p /var/www/pelican
-cd /var/www/pelican
+cat <<EOF > /etc/systemd/system/wings.service
+[Unit]
+Description=Wings Daemon
+After=docker.service
+Requires=docker.service
+PartOf=docker.service
 
-curl -L https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz | sudo tar -xzv
+[Service]
+User=root
+WorkingDirectory=/etc/pelican
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/local/bin/wings
+Restart=on-failure
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
 
-## Installing Composer
+[Install]
+WantedBy=multi-user.target
+EOF
 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+    whiptail --title "Next Steps" --msgbox "Now go back to my repository and follow the steps\nafter the Wing installation to finish the\nPelican Panel installation." 12 60
+    ;;
 
-sudo COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
+  3)
+    panel_domain=$(whiptail --title "Panel Domain" --inputbox "What is your Panel Domain" 10 60 3>&1 1>&2 2>&3)
 
-## Installing Certbot / SSL Certificates
+    node_domain=$(whiptail --title "Node Domain" --inputbox "What is your Node Domain" 10 60 3>&1 1>&2 2>&3)
 
-sudo apt install -y python3-certbot-apache
+    certbot_mail=$(whiptail --title "Certbot E-Mail" --inputbox "Please enter your E-Mail" 10 60 3>&1 1>&2 2>&3)
 
-certbot certonly --apache -d panel.kabel6499.de
-certbot certonly --apache -d node.kabel6499.de
+    root_pw=$(whiptail --title "Create DB Password" \
+    --passwordbox "What do you want as your Root Acc DB Password?" 10 60 3>&1 1>&2 2>&3)
 
-## Disable Default Site
+    panel_pw=$(whiptail --title "Create DB Password for Panel Acc" \
+    --passwordbox "What do you want as your Panel Acc DB Password?" 10 60 3>&1 1>&2 2>&3)
 
-a2dissite 000-default default-ssl 000-default-le-ssl
+    pelican_db=$(whiptail --title "Panel Database Name" --inputbox "What you want to name your Panel Databse?" 10 60 "panel" 3>&1 1>&2 2>&3)
 
-cd /etc/apache2/sites-available/pelican.conf
-wget https://raw.githubusercontent.com/kabel6499/pelican/main/pelican.conf
-cd /var/www/pelican
+    panel_db_user=$(whiptail --title "Panel DB Username" --inputbox "What should be your DB Username for your Panel" 10 60 "pelican" 3>&1 1>&2 2>&3)
 
-sudo a2ensite pelican.conf
-sudo a2enmod rewrite
-sudo a2enmod php8.4
+    whiptail --title "Overview" --msgbox "$(echo -e "Panel Domain: $panel_domain\nSSL Mail: $certbot_mail\nDatabase Type: MySQL\nPanel Database Password: $panel_pw\nPanel Database Name: $pelican_db\nRoot User DB Password: $panel_pw\nDatabase User: $panel_db_user")" 15 60
 
-sudo systemctl restart apache2
+    ## Update Debian Based Systems
+    apt update
+    apt upgrade -y
 
-php artisan p:environment:setup
+    ## Install Apache2
+    apt install apache2 -y
 
-sudo chmod -R 755 storage/* bootstrap/cache/
+    ## Install PHP 8.4 with all dependencies 
+    apt install software-properties-common -y
+    add-apt-repository ppa:ondrej/php -y
+    apt install -y php8.4 php8.4-fpm php8.4-gd php8.4-mysql php8.4-mbstring php8.4-bcmath php8.4-xml php8.4-curl php8.4-zip php8.4-intl php8.4-sqlite3 libapache2-mod-php8.4
 
-sudo chown -R www-data:www-data /var/www/pelican
+    ## Installing other dependencies for pelican
+    apt install curl -y
+    apt install tar -y
+    apt install unzip -y
 
-## Installing Wings
+    ## Installing Database Server
+    echo "mysql-server mysql-server/root_password password $root_pw" | sudo debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password $root_pw" | sudo debconf-set-selections
 
-curl -sSL https://get.docker.com/ | CHANNEL=stable sudo sh
+    apt install mysql-server -y
 
-curl -sSL https://get.docker.com/ | CHANNEL=stable sudo sh
+    mysql -u root -p"$root_pw" <<EOF
+CREATE DATABASE IF NOT EXISTS $pelican_db;
+CREATE USER IF NOT EXISTS '$panel_db_user'@'localhost' IDENTIFIED BY '$panel_pw';
+GRANT ALL PRIVILEGES ON $pelican_db.* TO '$panel_db_user'@'localhost';
+FLUSH PRIVILEGES;
+EOF
 
-sudo mkdir -p /etc/pelican /var/run/wings
-sudo curl -L -o /usr/local/bin/wings "https://github.com/pelican-dev/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
-sudo chmod u+x /usr/local/bin/wings
+    ## Creating Directory and Download Pelican Repo
+    sudo mkdir -p /var/www/pelican
+    cd /var/www/pelican
 
-echo now visit your-domain.com/installer and configure your Panel
+    curl -L https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz | sudo tar -xzv
+
+    ## Installing Composer
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+    sudo COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
+
+    ## Installing Certbot / SSL Certificates
+    sudo apt install -y python3-certbot-apache
+
+    certbot certonly --apache \
+    --non-interactive \
+    --agree-tos \
+    --redirect \
+    --email "$certbot_mail" \
+    -d "$panel_domain"
+
+    ## Disable Default Site
+    a2dissite 000-default default-ssl 000-default-le-ssl
+
+    cat <<EOF > /etc/apache2/sites-available/pelican.conf
+<VirtualHost *:80>
+    ServerName $panel_domain
+
+    RewriteEngine On
+    RewriteCond %{HTTPS} !=on
+    RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L] 
+</VirtualHost>
+
+<VirtualHost *:443>
+    ServerName $panel_domain
+    DocumentRoot "/var/www/pelican/public"
+
+    AllowEncodedSlashes On
+
+    php_value upload_max_filesize 100M
+    php_value post_max_size 100M
+
+    <Directory "/var/www/pelican/public">
+        Require all granted
+        AllowOverride all
+    </Directory>
+
+    SSLEngine on
+    SSLCertificateFile /etc/letsencrypt/live/$panel_domain/fullchain.pem
+    SSLCertificateKeyFile /etc/letsencrypt/live/$panel_domain/privkey.pem
+</VirtualHost>
+EOF
+
+    sudo a2ensite pelican.conf
+    sudo a2enmod ssl
+    sudo a2enmod rewrite
+    sudo a2enmod php8.4
+
+    sudo systemctl restart apache2
+
+    php artisan p:environment:setup
+
+    sudo chmod -R 755 storage/* bootstrap/cache/
+
+    sudo chown -R www-data:www-data /var/www/pelican
+
+    certbot certonly --apache \
+    --non-interactive \
+    --agree-tos \
+    --redirect \
+    --email "$certbot_mail" \
+    -d "$node_domain"
+
+    curl -sSL https://get.docker.com/ | CHANNEL=stable sudo sh
+
+    sudo mkdir -p /etc/pelican /var/run/wings
+    sudo curl -L -o /usr/local/bin/wings "https://github.com/pelican-dev/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+    sudo chmod u+x /usr/local/bin/wings
+
+cat <<EOF > /etc/systemd/system/wings.service
+[Unit]
+Description=Wings Daemon
+After=docker.service
+Requires=docker.service
+PartOf=docker.service
+
+[Service]
+User=root
+WorkingDirectory=/etc/pelican
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/local/bin/wings
+Restart=on-failure
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+        whiptail --title "Next Steps" --msgbox "Now go back to my repository and follow the steps\nafter the Wing installation to finish the\nPelican Panel installation." 12 60
+            ;;
+esac
